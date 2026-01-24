6e40fe35e54c4963f706310edd653a498d87bedd
Yunil-chaudhary-sherpa <yunil.in.tech.1@gmail.com>
2026-01-24 23:33:39 +0545

    add new

--- Introducing Lists/Organizing list/Exercise.py
+++ Introducing Lists/Organizing list/Exercise.py
@@ -1,21 +1,21 @@
 # Store the places you want to visit in a list and print it.Then print again with sorted() method and reverse metohd of sorted list.
-# Travel_country = ['Japan','Korea','Monaco','Europe','Hawaii']
-# print(Travel_country)
-# print(sorted(Travel_country))
-# print(sorted(Travel_country, reverse=True))
-# print(Travel_country)
+Travel_country = ['Japan','Korea','Monaco','Europe','Hawaii']
+print(Travel_country)
+print(sorted(Travel_country))
+print(sorted(Travel_country, reverse=True))
+print(Travel_country)
 
-# # Use reverse() to change the order of the list.
-# Travel_country.reverse()
-# print(Travel_country)
-# Travel_country.reverse()
-# print(Travel_country)
+# Use reverse() to change the order of the list.
+Travel_country.reverse()
+print(Travel_country)
+Travel_country.reverse()
+print(Travel_country)
 
-# # Use sort() method in the list and print the list then reverse the values in the list print again.
-# Travel_country.sort()
-# print(Travel_country)
-# Travel_country.sort(reverse=True)
-# print(Travel_country)
+# Use sort() method in the list and print the list then reverse the values in the list print again.
+Travel_country.sort()
+print(Travel_country)
+Travel_country.sort(reverse=True)
+print(Travel_country)
 
 # Create a list of 6 countries you want to visit and print it.
 Country = ['Japan','Russia','Monaco','Korea','Germany','UK']
