f3998dfadeb452dbc49a8425a67532a219baff15
Yunil-chaudhary-sherpa <yunil.in.tech.1@gmail.com>
2026-08-18 15:45:30 +0545

    Adding2

---
+++ Dictonaries/Working With Dictionaries/Dictionary of Similar Objects.py
@@ -0,0 +1,7 @@
+# A Dictionary of Similar Objects-
+'''Most editors have some functionality that helps you format extended lists and dictionaries.  Other acceptable ways to format long dictionaries are available as well, so you may see slightly different formatting in other sources.'''
+fav_lang = {'Maria':'python',
+            'Jen':'java',
+            'Gene':'C',
+            'Ali':'C++'}
+print("Maria's favourite language is "+fav_lang['Maria'].title()+'.')
