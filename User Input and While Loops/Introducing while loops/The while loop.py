1898866030ee9cb6c3f6abedc843bdd71d07028c
Yunil-chaudhary-sherpa <yunil.in.tech.1@gmail.com>
2026-09-05 17:31:01 +0545

    adding

--- User Input and While Loops/Introducing while loops/The while loop.py
+++ User Input and While Loops/Introducing while loops/The while loop.py
@@ -1,2 +1,7 @@
-# 
-char = []
+'''The while loop in action-
+If the end point of a loop is not known while loop is used.
+'''
+counter = 0
+while counter <= 4:
+  print("Loop number: "+str(counter))
+  counter = counter + 1
