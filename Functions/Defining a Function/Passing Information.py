f4ee7d355b2828e91a5ccce7b2662c3daffe3b6c
Yunil-chaudhary-sherpa <yunil.in.tech.1@gmail.com>
2026-09-15 22:47:57 +0545

    adding

--- Functions/Defining a Function/Passing Information.py
+++ Functions/Defining a Function/Passing Information.py
@@ -1,2 +1,12 @@
-'''Passing information to a function-
-'''
+'''Defining a funstion- 
+Keyword 'def' to inform Python that you're defining a function.'''
+def greet_user():
+  '''Display a simple greeting.'''
+  print('Hello!')
+greet_user()
+
+# Passing information to a function-
+def greet_user(username):
+  '''Display a simple greeting.'''
+  print('Hello, '+username.title()+'!')
+greet_user('kim')
